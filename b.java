a5f34eaec59cc56df144af3ba1831f9a5767b88b
BSP16SCM65H <bholavan@hawk.iit.edu>
2016-04-20 01:04:09 -0500

    Update b.java

--- b.java
+++ b.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< 3a4acff7209c6153ecc5e520bd236f28dffef425
 First Name Added
 
 BSP16SCM52D
@@ -10,9 +8,4 @@ BSP16SCM30S
 
 Last Name Added
 
-=======
-BSP16SCM65H
->>>>>>> Fixes #16. BSP16SCM65H first name added
-=======
-BSP16SCM65H
->>>>>>> 8c1830f7ef9be42d04bf0ca2900508ae24fa76d4
+

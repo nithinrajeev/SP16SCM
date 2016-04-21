1b0606d5da57c6758a681f0b2eb3ac998c88ecb0
ASP16SCM31G <agupta98@hawk.iit.edu>
2016-04-21 16:22:19 -0500

    Resolving conflict

--- v.java
+++ v.java
@@ -1,3 +1,4 @@
+<<<<<<< 6123227b1caa2ded1857ed6880a5a818c99e8adf
 <<<<<<< d0cd1b652321fbbb99bf26fe2fffb68493a27fae
 First Name Added
 
@@ -19,4 +20,7 @@ Last Name Added
 
 =======
 VSP16SCM25D
+>>>>>>> Fixes #75. VSP16SCM25D first name added”
+=======
+SP16SCM25D
 >>>>>>> Fixes #75. VSP16SCM25D first name added”

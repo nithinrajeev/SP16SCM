3a1801dc9036757cee68623e82f4537fdbd7080e
ASP16SCM31G <agupta98@hawk.iit.edu>
2016-04-21 16:32:28 -0500

    conflict resolution

--- v.java
+++ v.java
@@ -1,3 +1,4 @@
+<<<<<<< 4ffb437b8182195a91137222c7ed82322bcf4eac
 <<<<<<< 1b0606d5da57c6758a681f0b2eb3ac998c88ecb0
 <<<<<<< 6123227b1caa2ded1857ed6880a5a818c99e8adf
 <<<<<<< d0cd1b652321fbbb99bf26fe2fffb68493a27fae
@@ -27,4 +28,7 @@ SP16SCM25D
 >>>>>>> Fixes #75. VSP16SCM25D first name added”
 =======
 VSP16SCM25D
+>>>>>>> Fixes #75. VSP16SCM25D first name added”
+=======
+VSP16SCM25
 >>>>>>> Fixes #75. VSP16SCM25D first name added”
